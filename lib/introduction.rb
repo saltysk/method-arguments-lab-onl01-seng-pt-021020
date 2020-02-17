9ed9eb16eb75fd777d3921fd9bf5d99d52c81777
Leo Kim <sotysk@gmail.com>
2020-02-17 16:27:27 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,8 @@
+def introduction(name)
+  puts "Hi, my bame is #{name}."
+end
+
+
 def introduction_with_language(name, language)
 puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-
